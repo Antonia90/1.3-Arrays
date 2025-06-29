58b2271da66fe9f8cf5813d8d91c868481d94262
Antonia90 <antominnucci@gmail.com>
2025-06-29 23:22:59 +0200

    Optimización de la función y calculo del cubo

--- ejercicio1_nivel_3.php
+++ ejercicio1_nivel_3.php
@@ -1,13 +1,9 @@
 <?php
 declare(strict_types=1);
 
-function calculateCube(int $x) : int {
-    return $x * $x;
-}
-
 $numbers = [2, 4, 3, 6];
 
-$cube = array_map('calculateCube', $numbers);
+$cube = array_map(fn(int $x): int => pow($x, 3), $numbers);
 
 echo "<pre>";
 print_r($cube);
